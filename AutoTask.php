5c18e1fb055d79d7d67b4d56a1f0f85aea04387d
Angelo Vidrio <omgk45@gmail.com>
2015-02-11 21:52:54 -0800

    Update check

---
+++ AutoTask.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+namespace BlockLogger;
+
+
+use pocketmine\Server;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+
+
+class AutoTask extends PluginTask{
+	
+	
+	
+	
+    public function __construct(Main $plugin){
+        parent::__construct($plugin);
+        $this->plugin = $plugin;
+    }
+	
+    public function onRun($currentTick){
+    	$tables = $this->plugin->getAll();
+        if($this->plugin->dataExists($tables)) {
+            $this->plugin->killAll($tables);
+        }
+    }
+}
